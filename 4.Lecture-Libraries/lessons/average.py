336506afa56a2f60e8cbc827b8ff5440b211fcd0
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-25 17:40:10 +0000

    avg- explain

--- 4.Lecture-Libraries/lessons/average.py
+++ 4.Lecture-Libraries/lessons/average.py
@@ -1,5 +1,17 @@
-# Demonstrates statistics
+# average.py - Demonstrates statistics
 
 import statistics
 
+# Calculate and print the mean (average) of a list of values using the statistics module
 print(statistics.mean([100, 90]))
+
+# Explanation:
+# This code demonstrates the use of the statistics module to calculate the mean (average) of a list
+# of values. The statistics.mean() function takes a list of numeric values as input and returns the
+# calculated mean.
+
+# The import statement "import statistics" at the beginning of the script imports the statistics
+# module, which provides various statistical functions, including mean.
+
+# The print() statement is used to display the result of the statistics.mean() function, which
+# calculates the mean of the values [100, 90]. The result will be the average of these values.
