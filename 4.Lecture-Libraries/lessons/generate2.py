c521b7a8b9a28b92382f2355b76db90889f812e5
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-25 17:47:06 +0000

    gen2- explain

--- 4.Lecture-Libraries/lessons/generate2.py
+++ 4.Lecture-Libraries/lessons/generate2.py
@@ -1,6 +1,25 @@
-# Demonstrates randint
+# generate2.py - Demonstrates randint
 
+# Import the 'random' module
 import random
 
+# Use the 'randint' function to generate a random integer within a specified range
 number = random.randint(1, 10)
+
+# Print the generated random number
 print(number)
+
+# Explanation:
+# This code demonstrates the usage of the 'randint' function from the 'random' module to generate
+# a random integer within a specified range.
+
+# The 'import random' statement at the beginning of the script imports the 'random' module,
+# allowing you to access its various randomization functions.
+
+# The 'randint' function is used to generate a random integer between the two arguments provided
+# (inclusive). In this case, the range is from 1 to 10, so the generated number will be between 1
+# and 10, including both endpoints.
+
+# The generated random number is stored in the variable 'number'.
+
+# The 'print' statement is then used to display the generated random number on the console.
