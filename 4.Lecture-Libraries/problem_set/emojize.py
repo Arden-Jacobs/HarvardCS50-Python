0d96b3b0d7ede69f8792c0877e5030745b78bef1
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-26 11:53:15 +0000

    emojize explain

--- 4.Lecture-Libraries/problem_set/emojize.py
+++ 4.Lecture-Libraries/problem_set/emojize.py
@@ -1,3 +1,22 @@
+# emojize.py - Converts text input to emoji using the emoji module
+
+# Import the emoji module for working with emojis
 import emoji
+
+# Prompt the user for input
 emo = input("Input: ")
-print(emoji.emojize("Output: {}".format(emo),language='alias'))
+
+# Convert the input text to an emoji and print the output
+print(emoji.emojize("Output: {}".format(emo), language='alias'))
+
+# Explanation:
+# This script uses the emoji module to convert text input into corresponding emojis.
+
+# The import statement "import emoji" at the beginning of the script imports the emoji module, which provides
+# functionality for working with emojis.
+
+# The script prompts the user for input using the "input()" function and stores the input in the variable "emo".
+
+# The "emoji.emojize()" function is used to convert the input text to an emoji. The converted emoji string is
+# then printed along with the prefix "Output:" using the "print()" function. The "language='alias'" parameter is
+# specified to use alias language for emojis (e.g., ":smile:" instead of the actual emoji character).
