1cb9b6b3d93a7e6a5886b31f933a8ae3b6dc36ec
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-26 11:47:05 +0000

    sayings explain

--- 4.Lecture-Libraries/lessons/sayings0.py
+++ 4.Lecture-Libraries/lessons/sayings0.py
@@ -1,6 +1,18 @@
+# sayings0.py - Custom module for generating messages
+
+# Define a function to print a greeting message
 def hello(name):
     print(f"hello, {name}")
 
-
+# Define a function to print a goodbye message
 def goodbye(name):
     print(f"goodbye, {name}")
+
+# Explanation:
+# This custom module defines two functions for generating greeting and goodbye messages.
+
+# The function "hello(name)" takes a name as an argument and prints a message using an f-string,
+# welcoming the provided name with "hello."
+
+# The function "goodbye(name)" takes a name as an argument and prints a message using an f-string,
+# saying goodbye to the provided name with "goodbye."
