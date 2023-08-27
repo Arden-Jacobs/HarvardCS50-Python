e69e3cc5775ae0e8a701f4c788eb7a81bb229b39
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-27 11:45:57 +0000

    hello explain

--- 5.Lecture-Unit_Test/lessons/hello0.py
+++ 5.Lecture-Unit_Test/lessons/hello0.py
@@ -1,14 +1,30 @@
-# Function to be tested
+# hello0.py - Demonstrates a function to be tested
 
-
+# Main function to run the program
 def main():
+    # Prompt the user for input and store it in the variable "name"
     name = input("What's your name? ")
+    # Call the "hello()" function with the user's name as an argument
     hello(name)
 
-
+# Function to print a greeting message
 def hello(to="world"):
+    # Print the greeting message using the provided or default name
     print("hello,", to)
 
-
+# Check if the script is run directly
 if __name__ == "__main__":
+    # Call the main function to start the program
     main()
+
+# Explanation:
+# This script demonstrates the use of a function to be tested.
+
+# The main function "main()" runs the program. It prompts the user for input and stores it in the variable "name".
+# It then calls the "hello()" function, passing the user's name as an argument.
+
+# The "hello(to="world")" function takes an optional parameter "to" with a default value of "world". It prints a
+# greeting message using the provided name or the default "world" if no name is provided.
+
+# The "__name__ == '__main__':" check at the end of the script ensures that the main function is executed only
+# when the script is run directly, not when it's imported as a module.
--- 5.Lecture-Unit_Test/lessons/hello1.py
+++ 5.Lecture-Unit_Test/lessons/hello1.py
@@ -1,14 +1,30 @@
-# Has function return a str instead
+# hello1.py - Demonstrates a function returning a string
 
-
+# Main function to run the program
 def main():
+    # Prompt the user for input and store it in the variable "name"
     name = input("What's your name? ")
+    # Call the "hello()" function and print the returned greeting message
     print(hello(name))
 
-
+# Function to generate a greeting message and return it as a string
 def hello(to="world"):
+    # Generate and return the greeting message using an f-string
     return f"hello, {to}"
 
-
+# Check if the script is run directly
 if __name__ == "__main__":
+    # Call the main function to start the program
     main()
+
+# Explanation:
+# This script demonstrates the use of a function that returns a string as a greeting message.
+
+# The main function "main()" runs the program. It prompts the user for input and stores it in the variable "name".
+# It then calls the "hello()" function with the provided name and prints the returned greeting message.
+
+# The "hello(to="world")" function takes an optional parameter "to" with a default value of "world". It generates a
+# greeting message using an f-string and returns the message as a string.
+
+# The "__name__ == '__main__':" check at the end of the script ensures that the main function is executed only
+# when the script is run directly, not when it's imported as a module.
