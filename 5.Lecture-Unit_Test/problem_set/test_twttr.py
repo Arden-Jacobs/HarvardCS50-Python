859156ef1bab30588857cb79252183373884c0cc
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-27 17:04:38 +0000

    twttr explain

--- 5.Lecture-Unit_Test/problem_set/test_twttr.py
+++ 5.Lecture-Unit_Test/problem_set/test_twttr.py
@@ -1,19 +1,61 @@
+# test_twttr.py - Test script for the shorten function from twttr.py using pytest
+
+# Import the pytest library
 import pytest
+
+# Import the shorten function from twttr.py
 from twttr import shorten
 
+# Function to test the shorten function with word input
 def test_word():
+    # Test case 1: Check if shorten("big") returns "bg" using an assertion
     assert shorten("big") == "bg"
+    # Test case 2: Check if shorten("TWITTER") returns "TWTTR" using an assertion
     assert shorten("TWITTER") == "TWTTR"
 
+# Function to test the shorten function with numerical input (expecting TypeError)
 def test_num():
+    # Use pytest's "raises" context manager to check if TypeError is raised
     with pytest.raises(TypeError):
         shorten(7)
 
+# Function to test the shorten function with a single letter input
 def test_letter():
+    # Test case: Check if shorten("i") returns an empty string using an assertion
     assert shorten("i") == ""
 
+# Function to test the shorten function with input containing numbers
 def test_omit_num():
+    # Test case: Check if shorten("abc123") returns "bc123" using an assertion
     assert shorten("abc123") == "bc123"
 
+# Function to test the shorten function with input containing punctuation
 def test_omit_punc():
+    # Test case: Check if shorten("hello, world") returns "hll, wrld" using an assertion
     assert shorten("hello, world") == "hll, wrld"
+
+# Explanation:
+# This script is a test script for the shorten function from twttr.py using the pytest library.
+# It tests the functionality of the shorten function for different input scenarios.
+
+# The import statement "import pytest" at the beginning of the script imports the pytest library.
+
+# The import statement "from twttr import shorten" imports the shorten function from the twttr.py module.
+
+# The "test_word()" function contains test cases for word input. It checks whether the shorten function returns
+# the expected output for different words.
+
+# The "test_num()" function contains a test case for numerical input. It uses pytest's "raises" context manager
+# to check if a TypeError is raised when attempting to pass a number to the shorten function.
+
+# The "test_letter()" function contains a test case for a single letter input. It checks whether the shorten function
+# returns an empty string for a single letter.
+
+# The "test_omit_num()" function contains a test case for input containing both letters and numbers. It checks whether
+# the shorten function correctly removes vowels but retains numbers.
+
+# The "test_omit_punc()" function contains a test case for input containing punctuation. It checks whether the
+# shorten function removes vowels and punctuation while retaining other characters.
+
+# The script concludes by calling the "test_word()", "test_num()", "test_letter()", "test_omit_num()",
+# and "test_omit_punc()" functions to run the test cases.
--- 5.Lecture-Unit_Test/problem_set/twttr.py
+++ 5.Lecture-Unit_Test/problem_set/twttr.py
@@ -1,17 +1,45 @@
+# twttr.py - Script to remove vowels from a given input word
+
+# Main function to take user input, process it using the shorten function, and print the output
 def main():
+    # Prompt the user for input
     npt = input("Input: ")
+    # Call the shorten function to process the input and store the result
     output = shorten(npt)
+    # Print the processed output
     print(output)
 
+# Function to remove vowels from a given word
 def shorten(word):
+    # Define a string containing all vowels
     vowels = "aeiouAEIOU"
+    # Initialize an empty string to store the output
     output = ""
 
+    # Iterate through each character in the input word
     for char in word:
+        # Check if the character is not a vowel
         if char not in vowels:
+            # Add the character to the output string
             output += char
 
+    # Return the processed output
     return output
 
+# Check if the script is run directly
 if __name__ == "__main__":
+    # Call the main function to start processing
     main()
+
+# Explanation:
+# This script takes user input, processes it using the shorten function, and then prints the processed output.
+# The main function handles user input and output, while the shorten function removes vowels from the input word.
+
+# The main function "main()" prompts the user for input, calls the "shorten()" function to process the input,
+# and then prints the processed output.
+
+# The "shorten()" function iterates through each character in the input word. If the character is not a vowel,
+# it's added to the output string.
+
+# The "__name__ == '__main__':" check at the end of the script ensures that the main function is executed only
+# when the script is run directly, not when it's imported as a module.
