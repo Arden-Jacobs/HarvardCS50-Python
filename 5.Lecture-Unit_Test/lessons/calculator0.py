4ae5e3b5c732b25e5531c2bc0d79dfe49c0ecb79
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-27 11:43:51 +0000

    calculator explain

--- 5.Lecture-Unit_Test/lessons/calculator0.py
+++ 5.Lecture-Unit_Test/lessons/calculator0.py
@@ -1,13 +1,25 @@
-# Demonstrates defining a function with a return value
+# calculator0.py - Demonstrates defining a function with a return value
 
-
+# Main function to run the program
 def main():
+    # Prompt the user for input and store it in the variable x
     x = int(input("What's x? "))
+    # Call the square function and print the result
     print("x squared is", square(x))
 
-
+# Function to calculate the square of a number and return the result
 def square(n):
     return n * n
 
-
+# Call the main function to start the program
 main()
+
+# Explanation:
+# This script demonstrates the use of a function with a return value to calculate the square of a number.
+
+# The main function "main()" runs the program. It prompts the user for input and stores it in the variable "x".
+# It then calls the "square()" function to calculate the square of "x" and prints the result.
+
+# The "square(n)" function takes a parameter "n" and returns the square of the input number using the expression "n * n".
+
+# The script concludes by calling the main function to start the program execution.
--- 5.Lecture-Unit_Test/lessons/calculator1.py
+++ 5.Lecture-Unit_Test/lessons/calculator1.py
@@ -1,14 +1,28 @@
-# Demonstrates defining a function with a return value
+# calculator1.py - Demonstrates defining a function with a return value
 
-
+# Main function to run the program
 def main():
+    # Prompt the user for input and store it in the variable x
     x = int(input("What's x? "))
+    # Call the square function and print the result
     print("x squared is", square(x))
 
-
+# Function to calculate the square of a number and return the result
 def square(n):
     return n * n
 
-
+# Check if the script is run directly
 if __name__ == "__main__":
+    # Call the main function to start the program
     main()
+
+# Explanation:
+# This script demonstrates the use of a function with a return value to calculate the square of a number.
+
+# The main function "main()" runs the program. It prompts the user for input and stores it in the variable "x".
+# It then calls the "square()" function to calculate the square of "x" and prints the result.
+
+# The "square(n)" function takes a parameter "n" and returns the square of the input number using the expression "n * n".
+
+# The "__name__ == '__main__':" check at the end of the script ensures that the main function is executed only
+# when the script is run directly, not when it's imported as a module.
