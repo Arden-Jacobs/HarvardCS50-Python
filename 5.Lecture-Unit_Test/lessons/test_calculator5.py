3654cc04ca379ad2ffea265fb0c51133fb3c5995
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-27 11:47:41 +0000

    test_calculator explain

--- 5.Lecture-Unit_Test/lessons/test_calculator5.py
+++ 5.Lecture-Unit_Test/lessons/test_calculator5.py
@@ -1,9 +1,33 @@
+# test_calculator5.py - Test script for the square function from calculator5.py
+
+# Import the square function from calculator5.py
 from calculator5 import square
 
-
+# Function to test the square function using assertions
 def test_square():
+    # Test case 1: Check if square(2) returns 4 using an assertion
     assert square(2) == 4
+    # Test case 2: Check if square(3) returns 9 using an assertion
     assert square(3) == 9
+    # Test case 3: Check if square(-2) returns 4 using an assertion
     assert square(-2) == 4
+    # Test case 4: Check if square(-3) returns 9 using an assertion
     assert square(-3) == 9
+    # Test case 5: Check if square(0) returns 0 using an assertion
     assert square(0) == 0
+
+# Call the function to run the test cases
+test_square()
+
+# Explanation:
+# This script is a test script for the square function from calculator5.py. It tests the functionality of the square function.
+
+# The import statement "from calculator5 import square" at the beginning of the script imports the square function from
+# the calculator5.py module.
+
+# The "test_square()" function contains multiple test cases for the square function. It uses assertions to check whether
+# square(2) is equal to 4, whether square(3) is equal to 9, whether square(-2) is equal to 4,
+# whether square(-3) is equal to 9, and whether square(0) is equal to 0. If any of these assertions fail, an assertion
+# error will be raised.
+
+# The script concludes by calling the "test_square()" function to run the test cases.
--- 5.Lecture-Unit_Test/lessons/test_calculator6.py
+++ 5.Lecture-Unit_Test/lessons/test_calculator6.py
@@ -1,17 +1,45 @@
+# test_calculator6.py - Test script for the square function from calculator6.py
+
+# Import the square function from calculator6.py
 from calculator6 import square
 
-
+# Function to test positive input values
 def test_positive():
-    # assert square(1) == 1
+    # Test case 1: Check if square(2) returns 4 using an assertion
     assert square(2) == 4
+    # Test case 2: Check if square(3) returns 9 using an assertion
     assert square(3) == 9
 
-
+# Function to test negative input values
 def test_negative():
-    # assert square(-1) == 1
+    # Test case 1: Check if square(-2) returns 4 using an assertion
     assert square(-2) == 4
+    # Test case 2: Check if square(-3) returns 9 using an assertion
     assert square(-3) == 9
 
-
+# Function to test zero input value
 def test_zero():
+    # Test case: Check if square(0) returns 0 using an assertion
     assert square(0) == 0
+
+# Call the functions to run the test cases
+test_positive()
+test_negative()
+test_zero()
+
+# Explanation:
+# This script is a test script for the square function from calculator6.py. It tests the functionality of the square function.
+
+# The import statement "from calculator6 import square" at the beginning of the script imports the square function from
+# the calculator6.py module.
+
+# The "test_positive()" function contains test cases for positive input values. It uses assertions to check whether
+# square(2) is equal to 4 and whether square(3) is equal to 9.
+
+# The "test_negative()" function contains test cases for negative input values. It uses assertions to check whether
+# square(-2) is equal to 4 and whether square(-3) is equal to 9.
+
+# The "test_zero()" function contains a test case for the input value 0. It uses an assertion to check whether
+# square(0) is equal to 0.
+
+# The script concludes by calling the "test_positive()", "test_negative()", and "test_zero()" functions to run the test cases.
