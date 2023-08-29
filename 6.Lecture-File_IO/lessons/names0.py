e100a4dfbc783d5f41e871c631c9291825836b80
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-29 13:54:01 +0000

    name explain

--- 6.Lecture-File_IO/lessons/names0.py
+++ 6.Lecture-File_IO/lessons/names0.py
@@ -1,4 +1,12 @@
-# Stores a name in a variable
+# names0.py - Stores a name in a variable and prints a greeting message
 
+# Prompt the user to input their name and store it in the "name" variable
 name = input("What's your name? ")
+
+# Print a greeting message using the stored name
 print(f"hello, {name}")
+
+# Explanation:
+# This script prompts the user to input their name using the input() function and stores the input in the "name" variable.
+# The script then uses the f-string (formatted string) syntax to print a greeting message that includes the stored name.
+# The message "hello, {name}" is printed to the console, where {name} is replaced with the actual value stored in the "name" variable.
