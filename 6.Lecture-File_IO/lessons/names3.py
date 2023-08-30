a09b5a6b73ef9edf0ef4a5e484e0d7fdc82e01a1
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-08-30 12:04:53 +0000

    names explain

--- 6.Lecture-File_IO/lessons/names3.py
+++ 6.Lecture-File_IO/lessons/names3.py
@@ -1,7 +1,22 @@
-# Appends to a file
+# names3.py - Prompts for a name and appends it to a file
 
+# Prompt the user to input their name and store it in the "name" variable
 name = input("What's your name? ")
 
+# Open the "names.txt" file in append mode and append the input name to the file
 file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+file.write(f"{name}\n")  # Append the name to the file, followed by a newline character
+file.close()             # Close the file after appending
+
+# Explanation:
+# This script prompts the user to input their name using the input() function and stores the input in the "name" variable.
+
+# It then opens the "names.txt" file in append mode using the open() function with the mode "a".
+# This means that if the file already exists, new data will be added to the end of the file without affecting existing data.
+
+# The script uses the write() method to append the value of the "name" variable to the file.
+# The f-string is used to format the input name and include a newline character "\n" to separate each name.
+
+# After appending the name to the file, the close() method is called to close the file and ensure that data is saved.
+
+# As a result, the input name will be appended to the "names.txt" file, each name on a new line.
