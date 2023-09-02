d53469cddf8077deb43cc1be78671ff1d6c79134
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-02 13:50:18 +0000

    lines&pizza explain

--- 6.Lecture-File_IO/problem_set/lines.py
+++ 6.Lecture-File_IO/problem_set/lines.py
@@ -1,43 +1,70 @@
-# File Name: lines.py
-# Description: Count non-empty lines of code in a Python file
+# File: lines.py
+# Description: Counts the number of non-empty and non-comment lines in a Python source code file.
 
+# Import the sys module to access command-line arguments.
 import sys
 
-# Check the number of command-line arguments
+# Check the number of command-line arguments.
 if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
+# Get the first command-line argument (the filename).
 arg = sys.argv[1]
 
-# Check if the argument is a Python file
+# Check if the argument is a Python file (ends with ".py").
 if ".py" not in arg:
     sys.exit("Not a Python file")
 
+# Initialize a variable to count non-empty and non-comment lines.
 count = 0
 
 try:
+    # Open the Python file specified in the command-line argument.
     with open(arg) as file:
+        # Iterate through each line in the file.
         for line in file:
-            # Check if the line is empty or a comment line (starts with "#")
+            # Check if the line is either empty or starts with a comment (indicated by "# ").
             if line.strip() == "" or line.lstrip().startswith("# "):
+                # If the line is empty or a comment, skip it.
                 pass
             else:
+                # If the line is not empty and not a comment, increment the count.
                 count += 1
+    # Print the final count of non-empty and non-comment lines.
     print(count)
 
 except FileNotFoundError:
     sys.exit("File does not exist")
 
 # Explanation:
-# This script counts non-empty lines of code in a Python file.
-# It takes a Python file as a command-line argument and checks if the argument is a valid Python file.
 
-# The script uses a try-except block to handle possible errors, such as a non-existing file.
-# It opens the specified Python file using a with statement and iterates through each line in the file.
+# This script helps you count the number of lines that have actual code in a Python file. 
+# It doesn't count blank lines or lines that contain comments.
 
-# For each line, it checks if the line is either empty (whitespace characters removed) or a comment line
-# (starts with "# "). If the line is neither empty nor a comment, it increments the count variable.
+# To use the script, you should run it from the command line with the name of a Python file as an argument. 
+# For example, if you have a file named "my_program.py," you'd run the script like this:
+# python lines.py my_program.py
 
-# After processing all lines in the file, the script prints the count of non-empty lines of code.
+# The script first checks if you provided the correct number of arguments when running it. 
+# If you didn't provide the right number (either too few or too many), it will show an error message and exit.
+
+# Then, it checks if the provided argument ends with ".py" to ensure it's a Python file. 
+# If it's not, the script will also show an error message and exit.
+
+# After these initial checks, the script opens the Python file you specified. 
+# It goes through each line in the file one by one.
+
+# For each line, it checks if the line is empty or if it starts with a "#" symbol. 
+# In Python, "#" is used to mark comments. So, if a line is either empty or a comment, 
+# it's skipped and not counted.
+
+# If a line is not empty and doesn't start with "#" (meaning it contains actual code), 
+# the script increases the count of non-empty, non-comment lines by one.
+
+# After examining all lines in the file, the script prints out the final count, 
+# which represents the number of lines with actual code.
+
+# In summary, this script helps programmers quickly determine how much code is in a Python file 
+# without counting comments or blank lines manually.
--- 6.Lecture-File_IO/problem_set/pizza.py
+++ 6.Lecture-File_IO/problem_set/pizza.py
@@ -1,46 +1,65 @@
-# File Name: pizza.py
-# Description: Read and display data from a CSV file using the 'tabulate' library
+# File: pizza.py
+# Description: Reads a CSV file, converts it to a table, and prints it using the tabulate library.
 
+# Import necessary modules: sys for command-line arguments, csv for handling CSV files, and tabulate for tabular formatting.
 import sys
 import csv
 from tabulate import tabulate
 
-# Check the number of command-line arguments
+# Check the number of command-line arguments.
 if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
+# Get the first command-line argument (the filename).
 arg = sys.argv[1]
 
-# Check if the argument is a CSV file
+# Check if the argument is a CSV file (contains ".csv" in the filename).
 if ".csv" not in arg:
     sys.exit("Not a CSV file")
 
+# Initialize an empty list to store table data.
+table = []
+
 try:
-    table = []
-
+    # Open the specified CSV file.
     with open(arg) as file:
+        # Create a CSV reader that interprets the first row as column headers.
         reader = csv.DictReader(file)
+        # Iterate through each row in the CSV file.
         for row in reader:
+            # Append each row (represented as a dictionary) to the table list.
             table.append(row)
-
-        # Use the 'tabulate' library to display the data in a grid format
+        # Use the tabulate library to print the table in a grid format with headers.
         print(tabulate(table, headers="keys", tablefmt="grid"))
 
 except FileNotFoundError:
     sys.exit("File does not exist")
 
 # Explanation:
-# This script reads and displays data from a CSV file using the 'tabulate' library.
-# It takes a CSV file as a command-line argument and checks if the argument is a valid CSV file.
 
-# The script uses a try-except block to handle possible errors, such as a non-existing file.
-# It reads the data from the CSV file using the 'csv.DictReader' class, which treats the first row as headers.
+# This script reads a CSV file, converts its content into a table, and then prints the table in a grid format. 
+# It uses the `sys` module to access command-line arguments, the `csv` module to handle CSV files, and the `tabulate` library for formatting the table.
 
-# The data is stored in a list of dictionaries ('table') where each dictionary represents a row of data.
+# It begins by checking the number of command-line arguments provided when running the script. 
+# If there are too few or too many arguments, it displays an error message and exits.
 
-# After reading the data, the script uses the 'tabulate' library to format and display the data in a grid format.
-# The 'headers="keys"' argument specifies that the dictionary keys (column names) should be used as headers.
+# Next, it extracts the first command-line argument, which should be the filename of a CSV file. 
+# It checks if the argument contains ".csv" to ensure that it is indeed a CSV file. If not, it displays an error and exits.
 
-# Finally, the script prints the formatted table to the console.
+# Then, the script initializes an empty list named `table` to store the data from the CSV file.
+
+# It opens the specified CSV file and creates a CSV reader using `csv.DictReader`. 
+# This reader interprets the first row of the CSV file as column headers and treats each subsequent row as a dictionary.
+
+# The script then iterates through each row in the CSV file using a for loop. 
+# For each row, it appends the row (represented as a dictionary) to the `table` list.
+
+# Finally, it uses the `tabulate` library to print the table. 
+# It specifies "keys" for the headers parameter to use the dictionary keys (column names) as table headers. 
+# The `tablefmt` parameter is set to "grid" to format the table in a grid format.
+
+# If the specified file does not exist, the script displays an error message and exits.
+
+# In summary, this script provides an easy way to read and display the contents of a CSV file as a well-formatted table.
