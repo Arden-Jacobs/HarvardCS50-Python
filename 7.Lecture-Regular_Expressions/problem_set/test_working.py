aeeda1f2bd83cbbcf060d8a7088b2a1b27eb41c3
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-10 12:44:29 +0000

    working explain

--- 7.Lecture-Regular_Expressions/problem_set/test_working.py
+++ 7.Lecture-Regular_Expressions/problem_set/test_working.py
@@ -1,24 +1,78 @@
+# File Name: test_working.py
+
+# Description: This script contains test cases for the 'convert' function in the 'working' module.
+# The 'convert' function is designed to convert time ranges from a specific format to a standardized format.
+
+# Import necessary modules: convert from the 'working' module and pytest for testing.
+
 from working import convert
 import pytest
 
+# Define the 'test_format_error' function to test the handling of format errors.
 def test_format_error():
+    # Test case 1: An input with incorrect time range format, "09:00 AM - 17:00 PM," should raise a ValueError.
     with pytest.raises(ValueError):
         convert("09:00 AM - 17:00 PM")
+
+    # Test case 2: An input with incorrect time range format, "9 AM - 5 PM," should raise a ValueError.
     with pytest.raises(ValueError):
         convert("9 AM - 5 PM")
 
+# Define the 'test_incorrect_time_format' function to test the handling of incorrect time formats.
 def test_incorrect_time_format():
+    # Test case 1: An input with invalid hours and minutes, "09:90 AM - 17:70 PM," should raise a ValueError.
     with pytest.raises(ValueError):
-        convert("09:90 AM - 17:70 PM ")
+        convert("09:90 AM - 17:70 PM")
+
+    # Test case 2: An input with hours exceeding 12, "13 AM to 27 PM," should raise a ValueError.
     with pytest.raises(ValueError):
         convert("13 AM to 27 PM")
 
+# Define the 'test_correct_format' function to test the conversion of valid time ranges.
 def test_correct_format():
-    assert(convert("9 AM to 5 PM")) == "09:00 to 17:00"
-    assert(convert("9:00 AM to 5:00 PM")) == "09:00 to 17:00"
-    assert(convert("10 PM to 8 AM")) == "22:00 to 08:00"
-    assert(convert("10:30 PM to 8:50 AM")) == "22:30 to 08:50"
+    # Test case 1: A valid input with AM and PM, "9 AM to 5 PM," should be converted to "09:00 to 17:00."
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
 
+    # Test case 2: A valid input with hours and minutes, "9:00 AM to 5:00 PM," should be converted to "09:00 to 17:00."
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+
+    # Test case 3: A valid input with PM to AM transition, "10 PM to 8 AM," should be converted to "22:00 to 08:00."
+    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
+
+    # Test case 4: A valid input with hours and minutes, "10:30 PM to 8:50 AM," should be converted to "22:30 to 08:50."
+    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+
+# Define the 'test_noon_format' function to test the conversion of time ranges with noon.
 def test_noon_format():
-    assert(convert("12 AM to 12 PM")) == "00:00 to 12:00"
-    assert(convert("12:30 AM to 12:50 PM")) == "00:30 to 12:50"
+    # Test case 1: A valid input with noon, "12 AM to 12 PM," should be converted to "00:00 to 12:00."
+    assert convert("12 AM to 12 PM") == "00:00 to 12:00"
+
+    # Test case 2: A valid input with noon and minutes, "12:30 AM to 12:50 PM," should be converted to "00:30 to 12:50."
+    assert convert("12:30 AM to 12:50 PM") == "00:30 to 12:50"
+
+# Explanation:
+
+# Description: In this script, we have a collection of test cases for the 'convert' function, which is located in the 'working' module.
+# The purpose of these tests is to verify that the 'convert' function correctly converts time ranges from various formats to a standardized format.
+
+# We start by importing the 'convert' function from the 'working' module and the 'pytest' library for testing.
+
+# The script defines four test functions:
+
+# - 'test_format_error': This function tests the handling of format errors. It checks if the 'convert' function correctly raises
+#   'ValueError' when given inputs with incorrect time range formats.
+
+# - 'test_incorrect_time_format': This function tests the handling of incorrect time formats. It checks if the 'convert' function
+#   correctly raises 'ValueError' when given inputs with invalid hours and minutes.
+
+# - 'test_correct_format': This function tests the conversion of valid time ranges. It verifies that the 'convert' function correctly
+#   converts valid inputs to the expected standardized format.
+
+# - 'test_noon_format': This function tests the conversion of time ranges with noon. It checks if the 'convert' function correctly
+#   handles time ranges that involve noon.
+
+# Each test case includes assertions using 'pytest.raises' to check if the 'convert' function behaves as expected and returns the
+# correct results. If the function raises a 'ValueError' or returns the correct converted format, the test cases pass.
+
+# These test cases ensure that the 'convert' function accurately converts time ranges from various formats to a standardized format
+# and handles format errors and incorrect time formats appropriately.
--- 7.Lecture-Regular_Expressions/problem_set/working.py
+++ 7.Lecture-Regular_Expressions/problem_set/working.py
@@ -1,33 +1,87 @@
+# File Name: working.py
+
+# Description: This script defines a 'convert' function that converts time ranges from a specific format to a standardized format.
+# The script also includes a 'main' function to interactively input time ranges and display the converted format.
+
+# Import necessary modules: re for regular expressions.
+
 import re
 
-
+# Define the main function.
 def main():
+    # Get the user's input for the time range and display the converted format using the 'convert' function.
     print(convert(input("Hours: ")))
 
-
+# Define the 'convert' function to convert time ranges to a standardized format.
 def convert(s):
-    if matches := re.search(r"^(\d{1,2})(:[0-59]{2})? (AM|PM) to (\d{1,2})(:[0-59]{2})? (AM|PM)$",s):
+    # Use regular expressions to validate and parse the input time range.
+    if matches := re.search(
+        r"^(\d{1,2})(:[0-59]{2})? (AM|PM) to (\d{1,2})(:[0-59]{2})? (AM|PM)$", s
+    ):
+        # Extract individual components (hours, minutes, and noon) from the matched groups.
         hr1, min1, noon1, hr2, min2, noon2 = matches.groups()
+
+        # Check if the hours are greater than 12, which is not a valid format.
         if int(hr1) > 12 or int(hr2) > 12:
             raise ValueError
+
+        # Format and convert the times to a standardized format.
         time1 = time_format(hr1, min1, noon1)
         time2 = time_format(hr2, min2, noon2)
-        return(time1+' to '+time2)
+
+        # Return the converted time range in the standardized format.
+        return time1 + ' to ' + time2
     else:
+        # If the input does not match the expected format, raise a ValueError.
         raise ValueError
 
-
+# Define the 'time_format' function to format hours and minutes.
 def time_format(hrs, mint, noon):
+    # Convert hours to an integer.
     hrs = int(hrs)
+
+    # Adjust hours based on AM/PM and handle noon/midnight cases.
     if noon == "PM" and hrs != 12:
         hrs += 12
     elif noon == "AM" and hrs == 12:
         hrs = hrs - 12
-    if mint == None:
+
+    # Format the time string based on whether minutes are provided or not.
+    if mint is None:
         time = f"{hrs:02}:00"
     else:
         time = f"{hrs:02}{mint}"
+
     return time
 
+# Check if the script is run as the main program.
 if __name__ == "__main__":
+    # If it is, call the 'main' function to interactively input time ranges and display the converted format.
     main()
+
+# Explanation:
+
+# Description: This script defines a 'convert' function that is used to convert time ranges from a specific format to a standardized format.
+# The script also includes a 'main' function for interactive use, allowing the user to input time ranges and see the converted format.
+
+# We start by importing the 're' module for regular expressions.
+
+# The script defines three functions:
+
+# - 'main': This function is the entry point of the script. It gets the user's input for the time range and displays the converted format
+#   using the 'convert' function.
+
+# - 'convert': This function is responsible for validating and converting time ranges. It uses regular expressions to validate the input
+#   against a specific pattern. If the input matches the pattern, it extracts the individual components (hours, minutes, and noon)
+#   and checks if the hours are greater than 12 (not a valid format). If everything is valid, it calls the 'time_format' function to
+#   format and convert the times to a standardized format and returns the converted time range. If the input does not match the expected
+#   format, it raises a 'ValueError.'
+
+# - 'time_format': This function formats hours and minutes based on whether the input includes minutes or not. It adjusts hours for AM/PM
+#   and handles cases involving noon/midnight.
+
+# The script also checks whether it is executed as the main program. If it is, it initiates the interactive process by calling the 'main'
+# function, allowing the user to input time ranges and see the converted format.
+
+# These functions ensure that the 'convert' function accurately converts time ranges from the specified format to a standardized format,
+# handling validation and formatting appropriately.
