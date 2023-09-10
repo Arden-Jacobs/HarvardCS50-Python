67f5703c460da6c68c52752a30e48fe15c1a22d4
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-10 12:40:06 +0000

    numb3rs explain

--- 7.Lecture-Regular_Expressions/problem_set/numb3rs.py
+++ 7.Lecture-Regular_Expressions/problem_set/numb3rs.py
@@ -1,25 +1,53 @@
+# File Name: numb3rs.py
+
+# Description: Validates an IPv4 address by checking if it consists of four groups of numbers separated by periods.
+# Each group should be in the range of 0 to 255. The script uses regular expressions to validate the format and
+# additional logic to check the value of each group.
+
+# Import necessary modules: re for regular expressions.
+
 import re
 
-
+# Define the main function.
 def main():
+    # Get the user's input for the IPv4 address and remove leading/trailing spaces.
     print(validate(input("IPv4 Address: ").strip()))
 
-
+# Define the validate function to check the validity of the IPv4 address.
 def validate(ip):
+    # Use re.search with the pattern to check if the IPv4 address matches the expected format.
     if re.search(r"^([0-9]{1,3}\.)[0-9]{1,3}\.[0-9]{1,3}\.([0-9]{1,3})$", ip):
-        # If the conditions are met, print "Valid."
+        # If the conditions for the format are met, split the address into groups.
         temp = ip.split(".")
         count = 0
         for i in temp:
-            if i <= "255" and i >= "0":
+            # Check if each group is within the valid range (0 to 255).
+            if "0" <= i <= "255":
                 count += 1
+        # If all groups are within the valid range and there are exactly four groups, return True (Valid).
         if count == 4:
             return True
         else:
             return False
     else:
-        # If the conditions are not met, print "Invalid."
+        # If the IPv4 address does not match the expected format, return False (Invalid).
         return False
 
+# Check if the script is run as the main program.
 if __name__ == "__main__":
     main()
+
+# Explanation:
+
+# Description: This script validates an IPv4 address provided by the user by checking if it consists of four groups of numbers separated by periods.
+# Each group should be in the range of 0 to 255. The script uses regular expressions to validate the format and additional logic to check the value of each group.
+
+# The script starts by defining the main function, which gets the user's input for the IPv4 address and removes any leading or trailing spaces.
+
+# It also defines the validate function to check the validity of the IPv4 address. It uses re.search with the pattern to check if the IPv4 address matches the expected format.
+
+# If the conditions for the format are met, the address is split into groups, and each group is checked to ensure it is within the valid range (0 to 255).
+
+# If all groups are within the valid range and there are exactly four groups, the function returns True (Valid). Otherwise, it returns False (Invalid).
+
+# Finally, the script checks if it is run as the main program and calls the main function to start the validation process.
--- 7.Lecture-Regular_Expressions/problem_set/test_numb3rs.py
+++ 7.Lecture-Regular_Expressions/problem_set/test_numb3rs.py
@@ -1,12 +1,44 @@
+# File Name: test_numb3rs.py
+
+# Description: This script contains test cases for the 'validate' function in the 'numb3rs' module.
+# We will test various scenarios to ensure that the validation of IPv4 addresses functions correctly.
+
+# Import the 'validate' function from the 'numb3rs' module.
 from numb3rs import validate
 
+# Let's define a test case function to validate non-numeric inputs.
 def test_alpha():
-    assert(validate("cat") == False)
-    assert(validate("cat.dog.hat.mat") == False)
-    assert(validate("one.two.three.four") == False)
+    # We expect these inputs to be invalid since they contain non-numeric characters.
+    assert validate("cat") == False
+    assert validate("cat.dog.hat.mat") == False
+    assert validate("one.two.three.four") == False
 
-def testinput():
-    assert(validate("1.2.3.4")) == True
-    assert(validate("1.2.3")) == False
-    assert(validate("")) == False
-    assert(validate("255.290.350.500")) == False
+# Now, let's define a test case function to validate valid and invalid IPv4 addresses.
+def test_input():
+    # These inputs are valid IPv4 addresses and should return True.
+    assert validate("1.2.3.4") == True
+
+    # These inputs are invalid IPv4 addresses and should return False.
+    assert validate("1.2.3") == False
+    assert validate("") == False
+    assert validate("255.290.350.500") == False
+
+# Explanation:
+
+# Description: In this script, we have a collection of test cases for the 'validate' function, which is located in the 'numb3rs' module.
+# The purpose of these tests is to thoroughly evaluate the functionality of the IPv4 address validation.
+
+# We start by importing the 'validate' function from the 'numb3rs' module, which we want to test.
+
+# The script is organized into two main sections, each containing a test case function.
+
+# 1. The 'test_alpha' function aims to validate non-numeric inputs. We expect these inputs to be invalid IPv4 addresses because they contain non-numeric characters.
+#    - For instance, "cat" contains letters, and "cat.dog.hat.mat" contains non-numeric characters and multiple periods. Similarly, "one.two.three.four" is not a valid IPv4 address.
+
+# 2. The 'test_input' function focuses on validating both valid and invalid IPv4 addresses.
+#    - In the first assertion, "1.2.3.4" is a valid IPv4 address, so we expect the 'validate' function to return True.
+#    - The next assertion, "1.2.3," is not a valid IPv4 address because it lacks a fourth group. Therefore, it should return False.
+#    - An empty input string "" is not a valid IPv4 address, so we anticipate a False result.
+#    - Lastly, "255.290.350.500" is also invalid because it contains groups with values exceeding 255. We expect it to return False as well.
+
+# These test cases provide comprehensive coverage to ensure that the IPv4 address validation function behaves as expected.
