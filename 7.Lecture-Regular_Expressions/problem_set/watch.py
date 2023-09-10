6c40534b3f7047f967ca61e12ae17fc5b1b93581
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-10 12:45:43 +0000

    watch explain

--- 7.Lecture-Regular_Expressions/problem_set/watch.py
+++ 7.Lecture-Regular_Expressions/problem_set/watch.py
@@ -1,18 +1,54 @@
+# File Name: watch.py
+
+# Description: This script defines a 'parse' function to extract YouTube video links from HTML embed code.
+# The script also includes a 'main' function to interactively input HTML code and display the extracted video link.
+
+# Import necessary modules: re for regular expressions and sys for command-line arguments.
+
 import re
 import sys
 
-
+# Define the main function.
 def main():
+    # Get the user's input for HTML code and display the extracted video link using the 'parse' function.
     print(parse(input("HTML: ")))
 
-
+# Define the 'parse' function to extract YouTube video links from HTML embed code.
 def parse(s):
+    # Strip leading and trailing whitespace from the input.
     s = s.strip()
-    if matches := re.search('"https?://(?:www.)?youtube.com/embed/(\S+?)"',s):
+
+    # Use regular expressions to search for YouTube video embed code in the input.
+    if matches := re.search('"https?://(?:www.)?youtube.com/embed/(\S+?)"', s):
+        # Extract the video ID from the matched group and construct the YouTube video link.
         link = "https://youtu.be/" + matches.group(1)
         return link
     else:
+        # If no match is found, return None.
         return None
 
+# Check if the script is run as the main program.
 if __name__ == "__main__":
+    # If it is, call the 'main' function to interactively input HTML code and display the extracted video link.
     main()
+
+# Explanation:
+
+# Description: This script defines a 'parse' function used to extract YouTube video links from HTML embed code.
+# The script also includes a 'main' function for interactive use, allowing the user to input HTML code and see the extracted video link.
+
+# We start by importing the 're' module for regular expressions and the 'sys' module for command-line arguments (although it's not used here).
+
+# The script defines two functions:
+
+# - 'main': This function is the entry point of the script. It gets the user's input for HTML code and displays the extracted video link
+#   using the 'parse' function.
+
+# - 'parse': This function is responsible for searching the input HTML code for YouTube video embed code using regular expressions. If it
+#   finds a match, it extracts the video ID from the matched group and constructs a YouTube video link. If no match is found, it returns None.
+
+# The script also checks whether it is executed as the main program. If it is, it initiates the interactive process by calling the 'main'
+# function, allowing the user to input HTML code and see the extracted video link.
+
+# These functions ensure that the 'parse' function correctly extracts YouTube video links from HTML embed code, making it easier for users
+# to access the videos by providing direct links.
