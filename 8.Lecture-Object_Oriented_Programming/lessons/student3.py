95d380a616b3bc45d8ae1d765aee4523c15efbc6
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-11 09:35:36 +0000

    student explain

--- 8.Lecture-Object_Oriented_Programming/lessons/student3.py
+++ 8.Lecture-Object_Oriented_Programming/lessons/student3.py
@@ -1,16 +1,45 @@
-# Returns student as tuple, without unpacking it
+# File Name: student3.py
 
+# Description: This script defines a 'get_student' function to collect a student's name and house and return them as a tuple.
+# The 'main' function calls 'get_student' to collect the information and uses indexing to access the tuple elements for display.
 
+# Define the main function.
 def main():
+    # Call the 'get_student' function to collect the student's name and house as a tuple.
     student = get_student()
+    
+    # Display the student's name and house using indexed access to tuple elements and a formatted string.
     print(f"{student[0]} from {student[1]}")
 
-
+# Define the 'get_student' function to collect the student's name and house and return them as a tuple.
 def get_student():
+    # Collect the student's name and house using the 'input' function.
     name = input("Name: ")
     house = input("House: ")
+    
+    # Return the name and house as a tuple.
     return (name, house)
 
-
+# Check if the script is run as the main program.
 if __name__ == "__main__":
+    # If it is, call the 'main' function to collect input and display the student's information.
     main()
+
+# Explanation:
+
+# Description: This script defines a 'get_student' function that collects a student's name and house, returning them as a tuple.
+# The 'main' function calls 'get_student' to collect the information and uses indexing to access the tuple elements for display.
+
+# The script consists of the following components:
+
+# - 'main': This function is the entry point of the script. It calls 'get_student' to collect the student's name and house as a tuple,
+#   which is stored in the 'student' variable. It then uses indexed access to tuple elements to display the student's information
+#   using a formatted string.
+
+# - 'get_student': This function collects the student's name and house using the 'input' function, prompting the user for input.
+#   It then returns the name and house as a tuple.
+
+# The script also checks whether it is executed as the main program. If it is, it initiates the interactive process by calling the 'main'
+# function, allowing the user to input the student's name and house and displaying the information.
+
+# This script efficiently captures and displays information about a student using tuples and indexed access to tuple elements.
