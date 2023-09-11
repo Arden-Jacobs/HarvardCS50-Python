71158626518c0d6000a2a5307781b5973c70c400
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-11 09:29:51 +0000

    student explain

--- 8.Lecture-Object_Oriented_Programming/lessons/student0.py
+++ 8.Lecture-Object_Oriented_Programming/lessons/student0.py
@@ -1,5 +1,29 @@
-# Represents a student with multiple variables
+# File Name: student0.py
 
+# Description: This script represents a student with multiple variables, allowing the user to input the student's name and house,
+# and then displaying the information.
+
+# Define a variable 'name' and prompt the user to input the student's name.
 name = input("Name: ")
+
+# Define a variable 'house' and prompt the user to input the student's house.
 house = input("House: ")
+
+# Display the student's name and house using a formatted string.
 print(f"{name} from {house}")
+
+# Explanation:
+
+# Description: This script represents a student and allows the user to input the student's name and house. It then displays the student's
+# name and house using a formatted string.
+
+# The script starts by defining a variable 'name' and uses the 'input' function to prompt the user to input the student's name. The input
+# is stored in the 'name' variable.
+
+# Next, it defines a variable 'house' and again uses the 'input' function to prompt the user to input the student's house. The input is
+# stored in the 'house' variable.
+
+# Finally, the script uses the 'print' function to display the student's name and house using a formatted string, with the placeholders
+# '{name}' and '{house}' replaced by the actual values provided by the user.
+
+# This script provides a simple way to capture and display information about a student, including their name and house.
