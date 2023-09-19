bc838db08f91484f7c0618636c6965585a5f7ad2
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-19 18:00:00 +0000

    main

---
+++ 9.Lecture-Et_Cetera/problem_set/project.py
@@ -0,0 +1,17 @@
+from datetime import date
+import csv
+
+
+def main():
+    write_income_header()
+    write_expense_header()
+    income_array = get_income()
+    expense_array = get_expense()
+    income_total = get_total(income_array)
+    expense_total = get_total(expense_array)
+    print(f"Total Income: {income_total}")
+    print(f"Total Expenses: {expense_total}")
+
+    if expense_total and income_total != None:
+        net_income = income_total - expense_total
+        print(f"Net Income: {net_income}")
