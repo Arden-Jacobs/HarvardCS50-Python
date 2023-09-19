41a2b711e05cea6bceb5c75cff41213771e9d8b5
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-19 18:02:27 +0000

    income

--- 9.Lecture-Et_Cetera/problem_set/project.py
+++ 9.Lecture-Et_Cetera/problem_set/project.py
@@ -23,3 +23,23 @@ def income_journal(**kwargs):
 
 def expenses_journal(**kwargs):
     return(kwargs)
+
+
+def get_income():
+    today_date = date.today()
+    income = ["Income Statement"]
+    while True:
+        try:
+            if input("Would you like to create a new Income entery (Y or N): ") == "Y":
+                item = input("Enter child name: ")
+                source = input("Enter parent name: ")
+                amount = int(input("Enter amount: "))
+                income.append(income_journal(date=f"{today_date}", item=item, source=source, amount=amount))
+                write_income_enteries(today_date, item, source, amount)
+            else:
+                break
+            ...
+        except:
+            return False
+    return income
+
