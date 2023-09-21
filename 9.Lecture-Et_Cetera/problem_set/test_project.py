c07327774a443371876cafa241ad71a78d076721
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2023-09-21 17:15:41 +0000

    test

--- 9.Lecture-Et_Cetera/problem_set/test_project.py
+++ 9.Lecture-Et_Cetera/problem_set/test_project.py
@@ -1,10 +1,20 @@
-def test_function_1():
-    ...
+import pytest
+from project import (
+    income_journal,
+    expenses_journal,
+    get_total,
+    expenses_by_category,
+)
 
-
-def test_function_2():
-    ...
-
-
-def test_function_n():
-    ...
+# Test income_journal function
+def test_income_journal():
+    """
+    Test the income_journal function.
+    """
+    entry = income_journal(date="2023-09-21", item="Test Item", source="Test Source", amount=100)
+    assert entry == {
+        "date": "2023-09-21",
+        "item": "Test Item",
+        "source": "Test Source",
+        "amount": 100,
+    }
